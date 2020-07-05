fb5a139fded02aee0b494719714124d31e4018cd
Sveta-Pam-Pam <svetamashenko@yandex.ru>
2020-07-05 13:14:01 +0300

    Release

---
+++ C#/Factorial/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 2; i <=n; i++){
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial = " + factorial);
+        }
+    }
+}
---
+++ C#/Fibonachchi/Fibonachchi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonachchi
+{
+    class Program
+    {
+        public static int Fib(int arg)
+        {
+            int i = 3, frst = 1, scnd = 1, fibonachchi = 0;
+            while (i <= arg)
+            {
+                fibonachchi = frst + scnd;
+                frst = scnd;
+                scnd = fibonachchi;
+                i++;
+            }
+            return fibonachchi;
+        }
+        static void Main()
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fibonachchi;
+            fibonachchi = n == 0 ? 0 : (n == 1 ? 1 : (n > 1 ? Fib(n) : -1));
+            if (fibonachchi > 0)
+                Console.WriteLine(fibonachchi);
+            else
+                Console.WriteLine("Error");
+        }
+    }
+}
