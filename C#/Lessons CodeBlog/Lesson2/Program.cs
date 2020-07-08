bce0d69a866f656c55fa1760f83f6470c4053760
Sveta-Pam-Pam <svetamashenko@yandex.ru>
2020-07-08 03:40:45 +0300

    Release

---
+++ C#/Lessons CodeBlog/Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetBufferSize(Console.LargestWindowHeight, Console.LargestWindowWidth); 
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
